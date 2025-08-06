6c773411020f66d4cc25f4f7b710b310e10923cd
kevinlanalytics <lank@vcu.edu>
2025-08-06 19:50:10 -0400

    call it finish for parsing practice

--- Code_Practice/pgsql_practice_questions.sql
+++ Code_Practice/pgsql_practice_questions.sql
@@ -9,10 +9,12 @@ select count(e.patient_id) as patient_count, e.trial_id, e.status from enrollmen
 group by e.trial_id, e.status
 order by patient_count desc;
 
+
 -- Q3: Show patients who are not enrolled in any trials. (not in and subquery function)
 select patient_id, name from patients p
 where patient_id not in (select patient_id from enrollment) or 
 order by name;
+
 
 -- Q4: For each region, rank patients by birth_date (oldest = 1). (rank() over, partition by aggregation function)
 select p.region, p.name, p.birth_date
@@ -20,6 +22,7 @@ from patients p
 rank () over (partition by p.region order by p.birth_date) as rank
 from patients p
 order by p.region, rank;
+
 
 -- Q5: Find the trial(s) with the highest number of enrolled patients. (order by desc function)
 select e.trial_id, count(e.patient_id) as patient_count
@@ -30,6 +33,7 @@ order by patient_count desc
 
 -- Q6: List only the most recent enrollment for each patient. (desc function)
 select * from enrollment order by enroll_date desc;
+
 
 -- Q7: Write a query that returns each patient's name and a column trial_status where: (Case when function)
     -- "Active" if the enrollment status is ''Active''
@@ -43,6 +47,7 @@ select p.name,
     end as trial_status
 from patients p
 left join enrollment e on p.patient_id = e.patient_id;
+
 
 -- Q8: Write a query to list all enrolled patients and the trial condition, using 'Unknown Trial' if the trial condition is null. (COALESCE function on simple NULL handling)
     -- COALESCE returns the first non-null value in the list.
@@ -59,6 +64,7 @@ from enrollment e
 group by e.trial_id
 order by patient_count desc; -- use limit 1 to get the highest count only
 
+
 -- Q10 alter tables foreign key data type. to remove constraints, alter the two tables, add constraints back 
 begin; -- use begin to start a transaction and wrap it, it is similar to CTE in SQL Server, it creates a temporary transaction block until commit or rollback.
  alter table enrollment drop constraint enrollment_patient_id_fkey;
@@ -73,10 +79,13 @@ update patients
 set name = NULL 
 where patient_id = 3; -- when patient_id is varchar you will need to do patient_id = '3' instead of patient_id = 3
 
+
 -- Q12: finding NULL value in the columns 
 select trial_id, condition 
 from trials 
-where condition is NULL; -- or column_name is null; you can use AND or OR to combine multiple conditions
+where condition is NULL; -- or column_name is null; 
+-- you can + AND or OR to combine multiple conditions
+
 
 -- Q13: Insert and delete a patient from the patients table.
     -- insert 
@@ -87,8 +96,8 @@ delete from patients
 where patient_id = 9;
 
 
--- Bonus
-    -- Calculate average age of patients per region. when today is the 2025-08-06
+--- Bonus ---
+-- Calculate average age of patients per region. when today is the 2025-08-06
         -- calculate the age 
 select patient_id, age('2025-08-06', birth_date) from patients 
         -- calculate the average age per region 
@@ -98,7 +107,9 @@ select
 from patients
 group by region
 order by average_age desc;
-        -- calculate the date of birth and split the age years and months to another column
+
+
+-- calculate the date of birth and split the age years and months to another column
 select
     patient_id,
     birth_date,
@@ -107,7 +118,9 @@ select
     extract(month from age('2025-08-06', birth_date)) as age_months
 from patients
 order by age_years desc, age_months desc;
-        -- now subtract the age years with the average age years
+
+
+-- now subtract the age years with the average age years
 with average_age as (
     select
         coalesce(region, 'Unknown Region') as region,
